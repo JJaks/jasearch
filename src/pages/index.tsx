13d7920384dfca0769b55f045dbff2d1f4b9393f
JJaks <jaagopj@gmail.com>
2023-02-27 02:50:48 +0200

    fix: added support for automatic commit tests and some test pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,14 @@ import Link from "next/link"
 function HomePage() {
     return (
         // navbar with a title on the left side and then a search bar and about link on the right side NO TAILWIND
-        <nav>
-            <div>
-                <h1>JaSearch</h1>
+        <nav className="homepage-nav">
+            <div >
+                <h1 className="homepage-nav--h1">JaSearch</h1>
             </div>
             <div>
-                <input type="text" placeholder="Search" />
-                <Link href="/about">
-                    <a>About</a>
+                <input className="homepage-nav--search" type="text" placeholder="Search" />
+                <Link className="homepage-nav--link" href="/about">
+                    About
                 </Link>
             </div>
         </nav>
